470722dafd7b425d333b460ef62fdd4e121249d2
Fuad Ismani <muhdfuad.ismani@gmail.com>
2020-08-27 12:58:56 +0800

    Dashboard updated

---
+++ ABC Play App Project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void BackToDashboard()
+    {
+        SceneManager.LoadScene("Dashboard");
+        Debug.Log("BACK!");
+    }
+
+    public void PlayARCamera()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+}
